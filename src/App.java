6f13b3cc7ca8e5f8db6a462681d3682d681696f9
Suomijoki <henri.suomijoki@student.hamk.fi>
2024-10-23 11:36:00 +0300

    if lisäys

--- src/App.java
+++ src/App.java
@@ -2,7 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         int ika = 16;
     
-        
+        if (ika > 0 && ika < 18)
+    {    
+        System.out.println("Olet alaikäinen");
+        if (ika == 15) {
+            System.out.println("Saat ajaa mopoa");
+        }
+        if (ika >= 16){
+            System.out.println("Saat ajaa kevaria");
+        }
     
     
     
